fa678fd645461b8948e0fbb07c5f2756abd185de
DAFONEX <info@dafonex.com>
2025-09-03 20:58:41 +0400

    Исправление ошибок

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
-import { Slot } from 'expo-router';
+import { Stack } from 'expo-router';
 import { View } from 'react-native';
 
-export default function RootLayout() {
+export default function Layout() {
   return (
     <View style={{ flex: 1 }}>
-      <Slot />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </View>
   );
 }
