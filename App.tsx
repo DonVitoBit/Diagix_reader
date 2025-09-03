5dd8ce420b43ecfcfcf89ea3ea9239e3c39bb996
DAFONEX <info@dafonex.com>
2025-09-03 20:55:40 +0400

    Обновлены файлы конфигурации приложения: изменен файл app.json для установки схемы вместо иконки, упрощены компоненты App и Layout, обновлены зависимости в package.json и pnpm-lock.yaml, включая downgrade expo-router до версии 2.0.0. Удалены лишние импорты и улучшена структура кода.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,1 @@
-import { View } from 'react-native';
-import { Slot } from 'expo-router';
-
-export default function App() {
-  return (
-    <View style={{ flex: 1 }}>
-      <Slot />
-    </View>
-  );
-}
+import 'expo-router/entry';
